832fc967c20cf4a627b8794f84ab78862c43f2d4
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-04-26 16:09:25 +0900

    multiple orb, restrict jump count, and scrolling the scene

--- SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/GameScene.swift
@@ -10,10 +10,11 @@ import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    var foregroundNode : SKSpriteNode?
     var backgroundNode : SKSpriteNode?
     var playerNode : SKSpriteNode?
-    var orbNode : SKSpriteNode?
     
+    var impulseCount = 4
     let CollisionCategoryPlayer : UInt32     = 0x1 << 1
     let CollisionCategoryPowerUpOrb : UInt32 = 0x1 << 2
     
@@ -35,38 +36,65 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         backgroundNode!.position    = CGPoint(x: size.width / 2.0, y: 0.0)
         addChild(backgroundNode!)
         
+        // adding the foreground
+        foregroundNode = SKSpriteNode()
+        addChild(foregroundNode!)
+        
         // add the player
         playerNode = SKSpriteNode(imageNamed: "Player")
         playerNode!.physicsBody = SKPhysicsBody(circleOfRadius: playerNode!.size.width / 2)
         playerNode!.position = CGPoint(x: size.width / 2.0, y: 80.0)
-        playerNode!.physicsBody!.dynamic = true
+        playerNode!.physicsBody!.dynamic = false
         playerNode!.physicsBody!.linearDamping = 1.0
         playerNode!.physicsBody!.allowsRotation = false
         playerNode!.physicsBody!.categoryBitMask = CollisionCategoryPlayer
         playerNode!.physicsBody!.contactTestBitMask = CollisionCategoryPowerUpOrb
         playerNode!.physicsBody!.collisionBitMask = 0
-        addChild(playerNode!)
+        foregroundNode!.addChild(playerNode!)
         
-        // add the orb object
-        orbNode = SKSpriteNode(imageNamed: "PowerUp")
-        orbNode!.position = CGPoint(x: 150.0, y: size.height - 25)
-        orbNode!.physicsBody = SKPhysicsBody(circleOfRadius: orbNode!.size.width / 2.0)
-        orbNode!.physicsBody!.dynamic = false
-        orbNode!.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrb
-        orbNode!.physicsBody!.collisionBitMask = 0
-        orbNode!.name = "POWER_UP_ORB"
-        addChild(orbNode!)
+        // add power up orbs
+        var orbNodePosition = CGPointMake(playerNode!.position.x, playerNode!.position.y + 100)
+        for i in 0...19 {
+            var orbNode = SKSpriteNode(imageNamed: "PowerUp")
+            
+            orbNodePosition.y += 140
+            orbNode.position = orbNodePosition
+            orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
+            orbNode.physicsBody!.dynamic = false
+            orbNode.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrb
+            orbNode.physicsBody!.collisionBitMask = 0
+            orbNode.name = "POWER_UP_ORB"
+            
+            foregroundNode!.addChild(orbNode)
+        }
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        playerNode!.physicsBody!.applyImpulse(CGVectorMake(0.0, 40.0))
+        if !playerNode!.physicsBody!.dynamic {
+            playerNode!.physicsBody!.dynamic = true
+        }
+        
+        if impulseCount > 0 {
+            playerNode!.physicsBody!.applyImpulse(CGVectorMake(0.0, 40.0))
+            impulseCount--
+        }
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
         var nodeB = contact.bodyB!.node!
         
         if nodeB.name == "POWER_UP_ORB" {
+            impulseCount++
             nodeB.removeFromParent()
+        }
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        if playerNode!.position.y >= 180.0 {
+            backgroundNode!.position =
+                CGPointMake(self.backgroundNode!.position.x, -((self.playerNode!.position.y - 180.0)/8))
+            foregroundNode!.position =
+                CGPointMake(self.foregroundNode!.position.x, -(self.playerNode!.position.y - 180.0))
         }
     }
 }
